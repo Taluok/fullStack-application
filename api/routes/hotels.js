ec8bfee05152f9e464c6cb91fca8627e706c6c5e
Taluok <tanialuduenaok@gmail.com>
2024-06-13 13:02:11 -0300

    Autenthication (login+register)
    Autenthication (login+register)

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,52 +1,33 @@
 import express from "express";
-
+import {
+    countByCity,
+    countByType,
+    createHotel,
+    deleteHotel,
+    getHotel,
+    getHotelRooms,
+    getHotels,
+    updateHotel,
+} from "../controllers/hotel.js";
+import Hotel from "../models/Hotel.js";
+import { verifyAdmin } from "../utils/verifyToken.js"
 const router = express.Router();
 
 //CREATE
-router.post("/", async (req,res)=>{
-    const newHotel = new Hotel(req.body)
-    try{
-        const savedHotel = await newHotel.save()
-        res.status(200).json(savedHotel)
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
-//UPDATE
-router.put("/:id", async (req,res)=>{
-    try{
-        const updateHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body})
-        res.status(200).json(updateHotel)
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
-//DELETE
-router.delete("/:id", async (req,res)=>{
-    try{
-        const updateHotel = await Hotel.findByIdAndUpdate(req.params.id)
-        res.status(200).json("Hotel has been deleted")
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
-//GET
-router.get("/:id", async (req,res)=>{
-    try{
-        const hotel = await Hotel.findById(req.params.id)
-        res.status(200).json(hotel)
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
-//GET ALL
-router.get("/", async (req, res, next)=>{
+router.post("/", verifyAdmin, createHotel);
 
-    try{
-        const hotels = await Hotel.find()
-        res.status(200).json(hotels)
-    }catch(err){
-        next(err)
-    }
-})
-export default router
+//UPDATE
+router.put("/:id", verifyAdmin, updateHotel);
+//DELETE
+router.delete("/:id", verifyAdmin, deleteHotel);
+//GET
+
+router.get("/find/:id", getHotel);
+//GET ALL
+
+router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
+router.get("/room/:id", getHotelRooms);
+
+export default router;

1607367d96d205cf0598621db08779e5bb8d348e
Taluok <tanialuduenaok@gmail.com>
2024-06-14 13:21:43 -0300

    installing React App
    installing React App

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,24 +1,38 @@
-import express from "express";
-import {
-    updateUser,
-    deleteUser,
-    getUser,
-    getUsers,
-} from "../controllers/user.js";
-import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
+import User from "../models/User.js";
 
-const router = express.Router();
-
-//UPDATE
-router.put("/:id", verifyUser, updateUser);
-
-//DELETE
-router.delete("/:id", verifyUser, deleteUser);
-
-//GET
-router.get("/:id", verifyUser, getUser);
-
-//GET ALL
-router.get("/", verifyAdmin, getUsers);
-
-export default router;
+export const updateUser = async (req, res, next) => {
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            { $set: req.body },
+            { new: true }
+        );
+        res.status(200).json(updatedUser);
+    } catch (err) {
+        next(err);
+    }
+}
+export const deleteUser = async (req, res, next) => {
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted.");
+    } catch (err) {
+        next(err);
+    }
+}
+export const getUser = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.params.id);
+        res.status(200).json(user);
+    } catch (err) {
+        next(err);
+    }
+}
+export const getUsers = async (req, res, next) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (err) {
+        next(err);
+    }
+}
